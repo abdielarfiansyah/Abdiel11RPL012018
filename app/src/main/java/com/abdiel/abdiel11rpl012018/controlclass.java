aee7660a3fc37f3a3555a3f33797c515783cfe14
abdielarfiansyah <abdiel.arfiansyah2004@gmail.com>
2020-08-22 13:06:44 +0700

    add SP

---
+++ app/src/main/java/com/abdiel/abdiel11rpl012018/controlclass.java
@@ -0,0 +1,28 @@
+package com.abdiel.abdiel11rpl012018;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+public class controlclass extends AppCompatActivity {
+    private SharedPreferences sharedPreferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        sharedPreferences = getSharedPreferences("login", Context.MODE_PRIVATE);
+        if (sharedPreferences.getString("username", "").isEmpty() ||
+                sharedPreferences.getString("username", "").equalsIgnoreCase("")){
+            //login dulu
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            finish();
+        }else{
+            startActivity(new Intent(getApplicationContext(),Mainmenu.class));
+            finish();
+
+        }
+    }
+}
